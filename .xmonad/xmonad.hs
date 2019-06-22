09954a8c763eefa08b4f6e98dbdf9ccd801dec8c
b0b1 <35732793+0xM4N@users.noreply.github.com>
2019-06-22 10:31:46 -0300

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -164,7 +164,7 @@ myStartupHook = do
     spawn "xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'"
 
 -- STATUS BAR
-myRightBar  = "sleep 1;conky -c $HOME/.xmonad/scripts/bar | dzen2 -p -ta r -fn 'Exo 2:size=10' -fg '#FFFFFF' -bg '#111111' -x 850 -h 30 -y 4 -w 512"
+myRightBar  = "sleep 1;conky -c $HOME/.xmonad/bar | dzen2 -p -ta r -fn 'Exo 2:size=10' -fg '#FFFFFF' -bg '#111111' -x 850 -h 30 -y 4 -w 512"
 myLeftBar   = "sleep 1;dzen2 -p -ta l -fn 'Exo 2:size=10' -fg '#FFFFFF' -bg '#111111' -h 30 -x 4 -y 4 -w 860"
 
 -- CONFIG
