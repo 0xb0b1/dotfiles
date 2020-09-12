f902326eb0b863794b9bb5225cf55aee5548c640
0xb0b1 <vcente82@gmail.com>
2020-09-12 10:26:44 -0300

    MY EMACS

---
+++ .doom.d/custom.el
@@ -0,0 +1,49 @@
+(put 'customize-themes 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282828" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
+ '(custom-safe-themes
+   '("42692f39cca2beb984f6c9395c8a7edaf8a16b18deb3b3b9c219c816bbc53012" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" "05b3e98e2c2f8ef551797f70c6f55da1ba91988422d084583f277a07a2ce6f2c" "7bac80e973a20217e49587c6a0efa268d11c97c548a3eb3cd51b018f08b667aa" "44b319854b52e191db2dfb74430999fc05b07d91b3c3507160cdb7c6448b8c2a" "7fa3da49a1be8e91979bacb09b1d8fcb0d9c4c9dc4e5e881d1b52d3fc48c2737" "cf3d5d77679f7daed6a2c863e4f2e30427d5e375b254252127be9359957502ec" "b2f629e196018178211d4f02979a86dc09eaec5ba5ce566ed37ccd8b8eef9157" "82360e5f96244ce8cc6e765eeebe7788c2c5f3aeb96c1a765629c5c7937c0b5b" "57bd93e7dc5fbb5d8d27697185b753f8563fe0db5db245592bab55a8680fdd8c" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "f827e830497c787caabc81e989c68cec0e943eef3233eefec55ce5cf06a05ae6" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "2899018e19d00bd73c10c4a3859967c57629c58a955a2576d307d9bdfa2fea35" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
+ '(fci-rule-color "#4F4F4F")
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#8CD0D3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#7F9F7F"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#494949"))
+ '(objed-cursor-color "#CC9393")
+ '(package-selected-packages
+   '(neotree treemacs airline-themes doom-modeline powerline-evil spaceline-all-the-icons))
+ '(pdf-view-midnight-colors (cons "#DCDCDC" "#3F3F3F"))
+ '(rustic-ansi-faces
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(vc-annotate-background "#3F3F3F")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7F9F7F")
+    (cons 40 "#a4b48f")
+    (cons 60 "#cac99f")
+    (cons 80 "#F0DFAF")
+    (cons 100 "#eacfa4")
+    (cons 120 "#e4bf99")
+    (cons 140 "#DFAF8F")
+    (cons 160 "#dea3a0")
+    (cons 180 "#dd97b1")
+    (cons 200 "#DC8CC3")
+    (cons 220 "#d68eb3")
+    (cons 240 "#d190a3")
+    (cons 260 "#CC9393")
+    (cons 280 "#ab8080")
+    (cons 300 "#8a6d6d")
+    (cons 320 "#695b5b")
+    (cons 340 "#4F4F4F")
+    (cons 360 "#4F4F4F")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-group 'disabled nil)
