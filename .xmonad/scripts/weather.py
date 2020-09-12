8d55d1e1c7feed0fb7ef58b52204951f1fa1807d
0xb0b1 <vcente82@gmail.com>
2020-09-12 15:01:27 -0300

    Updated

--- .xmonad/scripts/weather.py
+++ .xmonad/scripts/weather.py
@@ -4,14 +4,12 @@ import urllib.request
 city = "Santarem"
 api_key = "c2f448a0a17e0daf2fb9d28418357fc3"
 
-try:
+try: 
     weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}".format(city, api_key)).read())[2:-1])
 
     info = weather["weather"][0]["description"].capitalize()
     temp = int(float(weather["main"]["temp"]) - 272.15)
 
-
     print("%s, %i°C" % (info, temp))
 except: 
-    print('!')
-
+    print("!")
