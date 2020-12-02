1718b7728e5fe26b7c45c713b87c253e07186524
0xb0b1 <vcente82@gmail.com>
2020-12-02 09:31:26 -0300

    ...

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -55,7 +55,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq fancy-splash-image (concat doom-private-dir "splash/doom-emacs-color.png"))
+;; (setq fancy-splash-image (concat doom-private-dir "splash/doom-emacs-color.png"))
 
 (require 'elcord)
 (elcord-mode)
