ceb799b95e24b7677c50fa108c9f88269d28b27d
0xb0b1 <vcente82@gmail.com>
2020-12-05 10:13:16 -0300

    ...

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" default))
  '(package-selected-packages
    '(elcord nodejs-repl js-react-redux-yasnippets dracula-theme doom-modeline atom-one-dark-theme)))
 (custom-set-faces
